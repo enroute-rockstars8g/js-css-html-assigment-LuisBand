5519d52acf27a587b6c2cc7d97d16c6a4420cd02
Luis Banderas <lbanderasbarrera@gmail.com>
2022-04-06 01:43:26 -0500

    All set to last commit

---
+++ challenge/index.js
@@ -0,0 +1,43 @@
+const fibonacci = (number) => {
+    const fib = [0, 1];
+
+    for(let i = 2; i <= number; i++){
+        fib[i] = fib[i - 1] + fib[i - 2] ;
+    }
+
+    return fib[number];
+}
+
+const factorial = (number) => {
+    if (number < 0) 
+            return -1;
+    else if (number == 0) 
+        return 1;
+    else {
+        return (number * factorial(number - 1));
+    }
+}
+
+const renderTaquitos = (number) => {
+    document.getElementById('results').innerHTML = "";
+
+    for(let i=0; i<number; i++){
+        let img = document.createElement("img");
+        img.src = "../assets/taquito.png";
+        document.getElementById('results').appendChild(img);
+    }
+}
+
+const getResult = () => {
+    let option = document.getElementById("combo-box").value;
+    let value = document.getElementById("input-number").value;
+
+    switch(option){
+        case "fibonacci":
+            renderTaquitos(fibonacci(value));
+            break;
+        case "exponential":
+            renderTaquitos(factorial(value));
+            break;
+    }
+}
